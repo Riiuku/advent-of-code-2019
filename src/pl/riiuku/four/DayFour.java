12f4f17254e45703371dabae05eae88ee3e6be2c
Lukasz <wegrzyniak.lukasz@gmail.com>
2020-08-10 22:00:57 +0200

    nicer code

--- src/pl/riiuku/four/DayFour.java
+++ src/pl/riiuku/four/DayFour.java
@@ -14,41 +14,14 @@ public class DayFour {
         calculate();
     }
 
-
-    //    It is a six-digit number.
-//    The value is within the range given in your puzzle input.
-//    Two adjacent digits are the same (like 22 in 122345).
-//    Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-//    256310-732736
-//    [0] = 2 3 4 5 6 7
-//    [1] = 3 4 5
-//    [2] = 2 3 4 5 6
-//    [3] = 3 4 5 6 7
-//    [4] = 1 2 3
-//    [5] = 0 1 2 3 4 5 6
     private static void calculate() throws IOException {
         Set<Integer> values = new HashSet<>();
-
-//        for (int i = 2; i <= 7; i++) {
-//            for (int j = 3; j <= 5; j++) {
-//                for (int k = 2; k <= 6; k++) {
-//                    for (int l = 3; l <= 7; l++) {
-//                        for (int m = 1; m <= 3; m++) {
-//                            for (int n = 0; n <= 6; n++) {
-//                                if (i <= j && j <= k && k <= l && l <= m && m <= n)
-//                                    values.add(i + "" + j + "" + k + "" + l + "" + m + "" + n);
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
 
         for (int i = 256310; i <= 732736; i++) {
             values.add(i);
         }
 
-        Set<Integer> get = new HashSet<>();
+        Set<Integer> clearedValues = new HashSet<>();
 
         for (int i = 0; i < 10; i++) {
             int finalI = i;
@@ -70,31 +43,26 @@ public class DayFour {
 
                         if (addable) {
 
-                            get.add(value);
+                            clearedValues.add(value);
                         }
                     });
         }
 
 
-        Set<Integer> lastOne = new HashSet<>();
-
-        get.forEach(value -> {
+        long sum = clearedValues.stream().filter(value -> {
             char[] sets = value.toString().toCharArray();
             List<Integer> result = new ArrayList<>();
 
             for (char set : sets) {
                 int count = 0;
                 for (char c : sets) {
-                    if(set == c)
-                        count ++;
+                    if (set == c)
+                        count++;
                 }
                 result.add(count);
             }
-
-            if(result.contains(2)) {
-                lastOne.add(value);
-            }
-        });
-        System.out.println(lastOne.size());
+            return result.contains(2);
+        }).count();
+        System.out.println(sum);
     }
 }
